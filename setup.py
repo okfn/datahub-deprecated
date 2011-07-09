c79c997d9d867a8c259bd968966cd81bbe2ca88d
Friedrich Lindenberg <friedrich@pudo.org>
2011-07-09 17:17:26 +0200

    basic python application skeleton

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='datadeck',
+      version=version,
+      description="A platform for data wranglers",
+      long_description="",
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Database"],
+      keywords='webstore rest api tables csv json',
+      author='Open Knowledge Foundation',
+      author_email='info@okfn.org',
+      url='http://okfn.org',
+      license='GPLv3',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
