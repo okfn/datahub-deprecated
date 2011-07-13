a20ca2b13a19bbe22a1a4bdc7d4d2f8565767878
Friedrich Lindenberg <friedrich@pudo.org>
2011-07-13 18:21:05 +0200

    introduce web interface for events, tests for get methods.

--- datahub/views/event_api.py
+++ datahub/views/event_api.py
@@ -12,3 +12,12 @@ def get(event):
     return jsonify(event)
 
 
+stream_api = Blueprint('stream_api', __name__)
+
+@stream_api.route('/<type>/<id>', methods=['GET'])
+def stream(type, id):
+    """ Get the latest events on the given event stream. """
+    events = logic.event.latest_by_stream(type, id)
+    return jsonify(events)
+
+
--- test/test_event.py
+++ test/test_event.py
@@ -31,6 +31,22 @@ class EventTestCase(unittest.TestCase):
         body = json.loads(res.data)
         assert body['type'] == 'resource_created'
 
+    def test_event_does_not_exist(self):
+        res = self.app.get('/api/v1/event/1022')
+        assert res.status.startswith("404"), res.status
+
+    def test_existing_event_stream(self):
+        res = self.app.get('/api/v1/stream/node/1')
+        assert res.status.startswith("200"), res.status
+        body = json.loads(res.data)
+        assert len(body)==1, body
+        assert body[0]['type'] == 'resource_created', body
+
+    def test_nonexisting_event_stream(self):
+        res = self.app.get('/api/v1/stream/node/1022')
+        assert res.status.startswith("200"), res.status
+        body = json.loads(res.data)
+        assert len(body)==0, body
 
 if __name__ == '__main__':
     unittest.main()
